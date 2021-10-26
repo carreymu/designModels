820bc8dcd3db28a999cf690ae54443a6c7a32755
muwei1984@163.com <muwei1984@163.com>
2021-10-26 22:15:02 +0800

    static simple factory model

--- src/main/java/models/factory/simpleFactory/PizzaStore.java
+++ src/main/java/models/factory/simpleFactory/PizzaStore.java
@@ -3,6 +3,10 @@ package models.factory.simpleFactory;
 public class PizzaStore {
     public static void main(String[] args) {
         new OrderPizza(new SimpleFactory());
-        System.out.println("退出");
+        System.out.println("退出1");
+        System.out.println("--------");
+
+        new OrderPizza2();
+        System.out.println("退出2");
     }
 }
--- src/main/java/models/factory/simpleFactory/SimpleFactory.java
+++ src/main/java/models/factory/simpleFactory/SimpleFactory.java
@@ -17,4 +17,21 @@ public class SimpleFactory {
         }
         return pizza;
     }
+
+    // 简单工厂模式 - 静态工厂模式
+    public static Pizza createPizza2(String orderType) {
+        Pizza pizza = null;
+        System.out.println("使用简单工厂模式2");
+        if (orderType.equals("greek")) {
+            pizza = new GreekPizza();
+            pizza.setName(" greek pizza");
+        } else if (orderType.equals("cheese")) {
+            pizza = new CheesePizza();
+            pizza.setName(" cheese pizza");
+        } else if (orderType.equals("pepper")) {
+            pizza = new PepperPizza();
+            pizza.setName(" pepper pizza");
+        }
+        return pizza;
+    }
 }
